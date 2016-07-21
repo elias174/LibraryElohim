29a3a995c19e3c46e85fdef198bc8872e14c90f7
SergioCardenasS <sergio.cardenas@ucsp.edu.pe>
2016-07-21 16:06:49 -0500

    Adding the view of Inventory

--- tab_views/InventoryTab.py
+++ tab_views/InventoryTab.py
@@ -35,66 +35,53 @@ class ResultsButtonGroup(QtGui.QButtonGroup):
             n += 4
 
 
-class Fast_Sale_Tab(QtGui.QWidget):
+class Inventory_Tab(QtGui.QWidget):
     change_table = QtCore.pyqtSignal()
 
     def __init__(self):
-        super(Fast_Sale_Tab, self).__init__()
+        super(Inventory_Tab, self).__init__()
 
         self.screenGeometry = QtGui.QApplication.desktop().availableGeometry()
         # Initialize Layout
 
         # Signal to check total
-        self.last_query = (session.query(Producto).limit(12).all())
-
         self.central_layout = QtGui.QGridLayout()
 
-        self.sale_group = QtGui.QGroupBox(str("Venta"), self)
-        self.results_group = QtGui.QGroupBox(str("Resultados Busqueda"), self)
+        self.product_group = QtGui.QGroupBox(str("Productos"), self)
+        self.search_group = QtGui.QGroupBox(str("Busqueda"), self)
 
-        self.central_layout.addWidget(self.sale_group, 0, 0)
-        self.central_layout.addWidget(self.results_group, 0, 1)
+        self.central_layout.addWidget(self.search_group, 0, 0)
+        self.central_layout.addWidget(self.product_group, 1, 0)
 
-        self.initialize_sale_group()
+        self.initialize_product_group()
         self.initialize_results_group()
-        self.product_total = 0
 
-        QtCore.QObject.connect(self.button_group, QtCore.SIGNAL("buttonClicked(int)"),
-                               self, QtCore.SLOT("ResultButtonClick(int)"))
+        #QtCore.QObject.connect(self.button_group, QtCore.SIGNAL("buttonClicked(int)"),
+        #                       self, QtCore.SLOT("ResultButtonClick(int)"))
+
         self.change_table.connect(self.update_total)
         self.setLayout(self.central_layout)
 
-    def initialize_sale_group(self):
+    def initialize_product_group(self):
         self.layout_line = QtGui.QFormLayout()
-        # First Line
-        # self.label_search = QtGui.QLabel("Buscar Producto:", self)
-        # self.edit_search = QtGui.QLineEdit(self)
-        # self.edit_search.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, 
-        #                                                  QtGui.QSizePolicy.Fixed))
-        # Second Line
         self.table_items = QtGui.QTableWidget(self)
-        self.table_items.setColumnCount(5)
+        self.table_items.setColumnCount(8)
         self.table_items.setRowCount(0)
+        self.NewProductoButton = QtGui.QPushButton("Agregar Nuevo Producto",self)
 
-        self.table_items.setHorizontalHeaderLabels(["Cantidad", "Producto",
-                                                    "P.Unidad", "P.Total",
-                                                    "Eliminar"])
+
+        self.table_items.setHorizontalHeaderLabels(['ID', 'Categoria', 
+                                                    'Nombre', 'Precio Compra',
+                                                    'Precio Venta', 'Stock', 
+                                                    'Detalle', 'Modificar'])
 
         #self.layout_line.addRow(self.label_search, self.edit_search)
         self.layout_line.addRow(self.table_items)
-        self.table_items.setFixedSize(500,400)
-
-        self.sale_group.setMaximumWidth(self.screenGeometry.width() / 2)
-        self.sale_group.setLayout(self.layout_line)
-
-        #third line, 4th line
-        self.total_line = QtGui.QLabel("0.00", self)
-        self.pay_line = QtGui.QLabel("0.00", self)
-        self.layout_line.addRow("Total: ", self.total_line)
-        self.layout_line.addRow("A pagar: ", self.pay_line)
+        self.layout_line.addRow(self.NewProductoButton)
         
-#        to connect the text_search
+        self.product_group.setLayout(self.layout_line)
 
+#        to connect the text_search
     def initialize_results_group(self):
         self.layout_line_results = QtGui.QFormLayout()
 
@@ -103,26 +90,18 @@ class Fast_Sale_Tab(QtGui.QWidget):
 
         self.layout_line_results.addRow(self.label_search, self.edit_search)
 
-        self.layout_results = QtGui.QGridLayout()
-        self.button_group = ResultsButtonGroup(self, self.last_query,
-                                               self.layout_results)
-        self.layout_line_results.addRow(self.layout_results)
-        self.results_group.setLayout(self.layout_line_results)
+        self.search_group.setLayout(self.layout_line_results)
 
+        #self.button_group = ResultsButtonGroup(self, self.last_query,
+        #                                       self.layout_results)
+        #self.layout_line_results.addRow(self.layout_results)
+        #self.results_group.setLayout(self.layout_line_results)
 
-        self.edit_search.textChanged.connect(self.on_search_edit_changed)
+        #self.edit_search.textChanged.connect(self.on_search_edit_changed)
         #self.edit_search_added.textChanged.connect(self.on_search_edit_added_changed)
 
     @QtCore.pyqtSlot(int)
     def ResultButtonClick(self, id):
-        if self.product_total==10:
-            msgBox = QtGui.QMessageBox()
-            msgBox.setText('Venta Rapida solo acepta 10 productos')
-            msgBox.addButton(QtGui.QPushButton('Aceptar'), QtGui.QMessageBox.YesRole)
-            msgBox.setWindowTitle("Numero maximo de Productos")
-            msgBox.exec_()
-            return
-
         product = session.query(Producto).get(id)
         product_name = str(product.nombre)
         items = self.table_items.findItems(product_name, QtCore.Qt.MatchExactly)
@@ -159,47 +138,6 @@ class Fast_Sale_Tab(QtGui.QWidget):
             for row in xrange(self.table_items.rowCount()):
                 if not (row in items):
                     self.table_items.hideRow(row)
-
-    def delete_row(self):
-        button = QtGui.qApp.focusWidget()
-        index = self.table_items.indexAt(button.pos())
-        self.table_items.removeRow(index.row())
-        self.product_total-=1
-        self.change_table.emit()
-
-    def add_product_table(self, product):
-        self.i = self.table_items.rowCount()
-
-        self.table_items.insertRow(self.i)
-
-        button = QtGui.QPushButton()
-        button.clicked.connect(self.delete_row)
-        button.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
-        button.setIcon(QtGui.QIcon('icons/delete-128.png'))
-
-        def quantity_changed(val):
-            spin = QtGui.qApp.focusWidget()
-            row = self.table_items.indexAt(spin.pos()).row()
-            qty = float(val)
-            new_value = str(float(self.table_items.item(row, 2).text()) * qty)
-            self.table_items.setItem(row, 3, QtGui.QTableWidgetItem(new_value))
-            self.change_table.emit()
-
-        spinbox = QtGui.QSpinBox()
-        spinbox.setValue(1)
-        spinbox.setMinimum(1)
-
-        spinbox.valueChanged.connect(quantity_changed)
-
-        self.table_items.setCellWidget(self.i, 0, spinbox)
-        self.table_items.setItem(self.i, 1,
-                                 QtGui.QTableWidgetItem(str(product.nombre)))
-        self.table_items.setItem(self.i, 2,
-                                 QtGui.QTableWidgetItem(str(product.precio_venta)))
-        self.table_items.setItem(self.i, 3,
-                                 QtGui.QTableWidgetItem(str(product.precio_venta)))
-        self.table_items.setCellWidget(self.i, 4, button)
-        self.change_table.emit()
 
     def update_total(self):
         index_price = 3
