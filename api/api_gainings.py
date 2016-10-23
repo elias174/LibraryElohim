78d6b09dac82aa1adf37400df2addaa0d172a1e9
SergioCardenasS <sergio.cardenas@ucsp.edu.pe>
2016-10-23 09:57:03 -0500

    Adding script of gainings

--- api/api_gainings.py
+++ api/api_gainings.py
@@ -21,15 +21,13 @@ def gainings_by_month(year, month = None, day = None):
 		day_query = '%'+str(year)+'%'+'%-'+str(month)+'-%'
 	else:
 		day_query = '%'+str(year)+'%'+'%-'+str(month)+'-%'+'%'+str(day)+'%'
-	print day_query
 	final_query = (session.query(Factura, Detalle.cantidad, Producto.id, Producto.precio_compra, Producto.precio_venta).join(Detalle, Factura.id==Detalle.factura).\
 					filter(Factura.fecha.like(day_query)).\
 					join(Producto, Detalle.producto==Producto.id)).all()
 	list_query = []
-	for que in range(len(final_query)):
-		print final_query[que].cantidad, final_query[que].id, final_query[que].precio_compra, final_query[que].precio_venta
-		result = {'cantidad':final_query[que].cantidad, 'producto':final_query[que].id, 
-				'p_total_compra':final_query[que].precio_compra, 'p_total_venta':final_query[que].precio_venta,
-				'utilidad':final_query[que].precio_venta-final_query[que].precio_compra}
+	for query_ in range(len(final_query)):
+		result = {'cantidad':final_query[query_].cantidad, 'producto':final_query[query_].id, 
+				'p_total_compra':final_query[query_].precio_compra, 'p_total_venta':final_query[query_].precio_venta,
+				'utilidad':final_query[query_].precio_venta-final_query[query_].precio_compra}
 		list_query.append(result)
 	return list_query
