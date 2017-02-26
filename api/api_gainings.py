210eb73ae5e9193a67feefa02918d166bcf32376
elias174 <elias.valencia@ucsp.edu.pe>
2017-02-26 05:45:01 -0500

    Fixed some of pep8 and modified structure calling for reports

--- api/api_gainings.py
+++ api/api_gainings.py
@@ -1,10 +1,5 @@
-import os, sys
-from datetime import datetime, timedelta, date
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy import *
-from sqlalchemy.ext.declarative import declarative_base
-# from models import *
 from models import *
+
 
 class GainingsApi:
 
@@ -19,29 +14,44 @@ class GainingsApi:
             day_query = '%'+str(year)+'%'+'%-'+str(month).zfill(2)+'-%'
 
         else:
-            day_query = '%'+str(year)+'%'+'%-'+str(month).zfill(2)+'-%'+'%'+str(day).zfill(2)+'%'
+            day_query = ('%'+str(year)+'%'+'%-'+str(month).zfill(2)+'-%'+'%' +
+                         str(day).zfill(2)+'%')
 
         if type == "Libreria":
             final_query = (
-                session.query(Factura, Detalle.cantidad, Producto.id, Producto.precio_compra, Producto.precio_venta).join(
-                    Detalle, Factura.id == Detalle.factura).filter(Factura.fecha.like(day_query)).join(
-                    Producto, Detalle.producto==Producto.id)).all()
+                session.query(
+                    Factura, Detalle.cantidad, Producto.id,
+                    Producto.precio_compra,
+                    Producto.precio_venta).join(
+                    Detalle, Factura.id == Detalle.factura).filter(
+                    Factura.fecha.like(day_query)).join(
+                    Producto, Detalle.producto == Producto.id)).all()
             list_query = []
             for query_ in range(len(final_query)):
-                result = {'cantidad':final_query[query_].cantidad, 'producto':final_query[query_].id,
-                        'p_total_compra':final_query[query_].precio_compra, 'p_total_venta':final_query[query_].precio_venta,
-                        'utilidad':final_query[query_].precio_venta-final_query[query_].precio_compra}
+                result = {
+                    'cantidad': final_query[query_].cantidad,
+                    'producto': final_query[query_].id,
+                    'p_total_compra': final_query[query_].precio_compra,
+                    'p_total_venta': final_query[query_].precio_venta,
+                    'utilidad': (final_query[query_].
+                                 precio_venta-final_query[query_].precio_compra)
+                }
                 list_query.append(result)
             self.result_query = list_query
         elif type == "Servicios":
             final_query = (
-                session.query(Factura, Factura.cliente ,Servicio.tipo, Detalle.precio_total).join(
-                    Detalle, Factura.id == Detalle.factura).filter(Factura.fecha.like(day_query)).join(
-                    Servicio, Detalle.servicio == Servicio.id)).all()
+                session.query(Factura, Factura.cliente,
+                              Servicio.tipo, Detalle.precio_total).join(
+                              Detalle, Factura.id == Detalle.factura).filter(
+                              Factura.fecha.like(day_query)).join(
+                              Servicio, Detalle.servicio == Servicio.id).all()
+            )
             list_query = []
             for query_ in range(len(final_query)):
-                result = {'cliente':final_query[query_].cliente,'servicio':final_query[query_].tipo,
-                        'utilidad':final_query[query_].precio_total}
+                result = {
+                    'cliente': final_query[query_].cliente,
+                    'servicio': final_query[query_].tipo,
+                    'utilidad': final_query[query_].precio_total}
                 list_query.append(result)
             self.result_query = list_query
             
